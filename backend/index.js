5f1fab8ad647a5a2b3373d6244447bdeb88acdb5
Rahul Kumar Mishra <mishrarahul32299@gmail.com>
2026-02-07 23:36:37 +0530

    cors3

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://studmaneger-ekex.vercel.app/",
+      "https://studmaneger-ekex.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
